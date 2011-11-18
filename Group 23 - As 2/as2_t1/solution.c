d0a49e7e806cf705162e9a1a4a653e0cbabe267e
Marcel Boersma <boersma.marcel@gmail.com>
2011-11-18 14:18:31 +0000

    Not working!!!!

--- Group 23 - As 2/as2_t1/solution.c
+++ Group 23 - As 2/as2_t1/solution.c
@@ -1,20 +1,57 @@
 #include "as2_t1.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+typedef struct node {
+	struct node* next;
+} node;
+
+int initialized = 0;
+node * prev;
+node * curr;
+node * head;
 
 void *task_alloc(void) {
-	
 
-	task_t * p;
-	int i;
-
-	if (counter < (MEM_BLOCK_SIZE / sizeof(task_t))) { 
-		counter += 1;
+	printf("HIHIHIHIHIHIHIHIHI\n");
 	
-		return (task_t *)get_MEM_BLOCK_START() + counter;
-	} else {
-		return NULL;
-	}
+	if (initialized == 0) {
+		printf("HI DOES THIS HAPPEN AT ALL\n");
+		curr = (node *)((unsigned int)get_MEM_BLOCK_START() + MEM_BLOCK_SIZE - sizeof(task_t));
+		prev = NULL;
+		curr->next = (node *)((unsigned int)get_MEM_BLOCK_START());
+
+		printf("I have a small hunch this doesn't fuck things up\n");
+	
+		for (; (unsigned int)curr >= (unsigned int)get_MEM_BLOCK_START() + 2*sizeof(task_t); prev=curr, curr = (node *)((unsigned int)curr - sizeof(task_t))) {
+			/* printf("%p \n", main_pointer); */
+/*		curr->next = (node *)((unsigned int)get_MEM_BLOCK_START());*/	
+		curr->next = prev;
+/*	printf("%p %p %p\n", curr, curr->next, prev);*/
+			/*curr->next = (node *)prev;*/
+/*			printf("%p -> %p\n", head, head->next);*/ 
+		}
+
+		printf("Finished looping yo \n");
+
+		printf("HI HIHIHIHIHI\n");
+		head = (node *)((unsigned int)get_MEM_BLOCK_START());
+	
+		printf("Einde: %p\n", head);	
+		initialized = 1;
+	} 
+
+	printf("%p \n", curr);
+	printf("%p \n", head);
+
+	
+	curr = head->next;
+	/*if ((unsigned int)curr == (unsigned int)NULL) {
+		head->next = curr->next;
+	}*/
+
+	return curr;		
 }
 
 void task_free(void *ptr) {
@@ -22,4 +59,4 @@ void task_free(void *ptr) {
 		
 	}	
 } 
-}
+
