b1a1e174409202e97b93557affb1541c3334da9a
Cas Ebbers <cas@casebbers.net>
2011-12-01 16:01:59 +0100

    piThreads update

--- Group 23 - As 1/as1_t1/piThreads.c
+++ Group 23 - As 1/as1_t1/piThreads.c
@@ -1,67 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include <pthread.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <math.h>
+#include <time.h>
 
 /*For mutex lock*/
-pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 long double pi = 0; /*This is shared info between threads...because they share resources*/
 void *piCalculator(void *param); /* the calculating thread */
 void *piPrinter(void *param); /*printing thread*/
+int maxIterations;
+int maxExecTime;
+clock_t start, end;
+int iteration = 0;
 
 int main(int argc, char* argv[])
 {
-	printf("Welcome to the awesome pi calculator!!!\n");
-	printf("We'll do some calculating pi and displaying results in several threads.\n\n");
-	
-	
-	pthread_t calculate_pi_thread, print_pi_value_thread; /*Thread id*/
-	pthread_attr_t attr; /*Thread attributes*/
+	pthread_t      worker_thread, printer_thread;
 
-	pthread_attr_init(&attr); /*Set the attributes for the thread*/
-	pthread_create(&calculate_pi_thread, &attr, piCalculator, NULL);
-	pthread_create(&print_pi_value_thread, &attr, piPrinter, NULL);
+	if(argc != 3) {
+		printf("Usage: %s <max iterations> <max execution time>\n", argv[0]);
+		return 0;
+	}
 
-	
-	pthread_join(calculate_pi_thread, NULL);
-	pthread_join(print_pi_value_thread, NULL);	
+	maxIterations = atoi(argv[1]);
+	maxExecTime   = atoi(argv[2]) - 1;
 
-	printf("Done pi is calculated!!!\n\n");
+	start = clock();
 
+	pthread_create( &worker_thread,  NULL, piCalculator, NULL);
+	pthread_create( &printer_thread, NULL, piPrinter,    NULL);
+	// now we wait...
+	pthread_join(worker_thread, NULL);
+	pthread_mutex_destroy( &mutex );
 
-	return 1;
+	end = clock();
 
+	printf("\nexecution time = %.2fs\n", (((double)end)-((double)start))/CLOCKS_PER_SEC);
+	printf("iterations     = %d\n", iteration);
+	printf("pi             = %1.60Lf\n", 4*pi);
+
+	return 0;
 }
 
 /*The printing thread*/
 void *piPrinter(void *param)
 {
-	bool print_value = true;
-	
-	while(print_value){
-		printf("The current value of pi :  %1.65Lf\n", 4*pi);
+	while(1) {
+		printf("Iteration (%10d): %1.60Lf\n", iteration, 4*pi);
 		sleep(1);
 	}
-	/*Sleep for some milli seconds*/
-	pthread_exit(0);
 }
 
 /*The thread will begin in this function*/
 void *piCalculator(void *param)
 {
-	int i = 0;
-	while(true)
+	while(iteration < maxIterations)
 	{
+		if( ((clock()-start)/CLOCKS_PER_SEC) > maxExecTime ) {
+			printf("Time limit reached!\n");
+			pthread_exit(NULL);
+		}
+
 		/*Thread locks on mutex1 */
-		pthread_mutex_lock( &mutex1 );
-		/*calculate pi/4 */
-		pi = pi + ( pow(-1, i)/(2*i+1) );
+		pthread_mutex_lock( &mutex );
+		/*calculate pi */
+		pi = pi + ( pow(-1, iteration)/(2*iteration+1) );
 		/*Done calculating so unlock on mutex1*/
-		pthread_mutex_unlock( &mutex1 );	
-		i++;	
+		pthread_mutex_unlock( &mutex );
+
+		iteration++;
 	}
 
-	pthread_exit(0);
+	printf("Reached iteration limit!\n");
+	pthread_exit(NULL);
 }
