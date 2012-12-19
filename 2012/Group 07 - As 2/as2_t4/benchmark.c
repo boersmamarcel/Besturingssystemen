39d702f73ac3b71f3658e9bcbb3ff86a73c1f885
Bram <aerkenbrand@gmail.com>
2012-12-19 13:03:00 +0100

    Implemented benchmark It works...

--- 2012/Group 07 - As 2/as2_t4/benchmark.c
+++ 2012/Group 07 - As 2/as2_t4/benchmark.c
@@ -6,25 +6,45 @@
 
 #define LOOPS 1000*1000
 
+void *printResult(struct timeval start, struct timeval end) {
+    int result;
+    result = end.tv_usec - start.tv_usec;
+    if(result < 0)
+        result += 1000000;
+	printf("Time: %ld microsec\n\n", result);
+}
+
 int main(int argc, char **argv) {
 	int i;
 	task_t *tasks[LOOPS];
-	struct timeval startTime, endTime;
-	gettimeofday(&startTime, NULL);
-	
+	struct timeval startTime1, endTime1;
+    struct timeval startTime2, endTime2;
+    void *blub[LOOPS];
+   
+    printf("Benchmarking Malloc: \n");
+	gettimeofday(&startTime1, NULL);
 	for(i = 0; i < LOOPS; i++) {
 		tasks[i] = malloc(sizeof(task_t));
-		/*tasks[i] = task_alloc();*/
 	}
-	
+    for(i = 0; i < LOOPS; i++) {
+        free(tasks[i]);
+    }
+	gettimeofday(&endTime1, NULL);
+    printResult(startTime1, endTime1);
+
+
+    printf("Benchmarking our solution: \n"); 
+	gettimeofday(&startTime2, NULL);
 	for(i = 0; i < LOOPS; i++) {
-		free(tasks[i]);
-		/*tasks[i] = task_alloc();*/
+        tasks[i] = task_alloc();
+    }
+    /* Now free everything again*/
+    for(i = 0; i < LOOPS; i++) {
+	    task_free(tasks[i]);
 	}
+	gettimeofday(&endTime2, NULL);
+    printResult(startTime2, endTime2);
 	
-	gettimeofday(&endTime, NULL);
-	
-	printf("Time: %ld\n", (endTime.tv_usec-startTime.tv_usec));
 	
 	return 0;
 }
