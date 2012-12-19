9dd3750dd51a088d5dfcf6bcfef1a41b1775d33c
Bram <aerkenbrand@gmail.com>
2012-12-19 13:09:10 +0100

    Added useless comments...

--- 2012/Group 07 - As 2/as2_t4/benchmark.c
+++ 2012/Group 07 - As 2/as2_t4/benchmark.c
@@ -7,44 +7,46 @@
 #define LOOPS 1000*1000
 
 void *printResult(struct timeval start, struct timeval end) {
-    int result;
-    result = end.tv_usec - start.tv_usec;
-    if(result < 0)
-        result += 1000000;
+	int result;
+	result = end.tv_usec - start.tv_usec;
+	   
+	if(result < 0) /* Protect against "underflows" */
+		result += 1000000; /* Add one second */
+	
+	/* Print the result */
 	printf("Time: %ld microsec\n\n", result);
 }
 
 int main(int argc, char **argv) {
-	int i;
-	task_t *tasks[LOOPS];
-	struct timeval startTime1, endTime1;
-    struct timeval startTime2, endTime2;
-    void *blub[LOOPS];
-   
-    printf("Benchmarking Malloc: \n");
+	int i; /* Our counter */
+	task_t *tasks[LOOPS]; /* Array of rubbish to test with */
+	struct timeval startTime1, endTime1; /* Clocks (could have been re-used */
+	struct timeval startTime2, endTime2;
+
+	/* Testing Malloc, for reference */
+	printf("Benchmarking Malloc: \n");
 	gettimeofday(&startTime1, NULL);
 	for(i = 0; i < LOOPS; i++) {
 		tasks[i] = malloc(sizeof(task_t));
 	}
-    for(i = 0; i < LOOPS; i++) {
-        free(tasks[i]);
-    }
+	for(i = 0; i < LOOPS; i++) {
+		free(tasks[i]);
+	}
 	gettimeofday(&endTime1, NULL);
-    printResult(startTime1, endTime1);
+	printResult(startTime1, endTime1);
 
-
-    printf("Benchmarking our solution: \n"); 
+	/* Testing our solution; see how well we did */
+	printf("Benchmarking our solution: \n"); 
 	gettimeofday(&startTime2, NULL);
 	for(i = 0; i < LOOPS; i++) {
-        tasks[i] = task_alloc();
-    }
-    /* Now free everything again*/
-    for(i = 0; i < LOOPS; i++) {
-	    task_free(tasks[i]);
+		tasks[i] = task_alloc();
+	}
+	for(i = 0; i < LOOPS; i++) {
+		task_free(tasks[i]);
 	}
 	gettimeofday(&endTime2, NULL);
-    printResult(startTime2, endTime2);
-	
-	
+	printResult(startTime2, endTime2);
+
 	return 0;
+	/* Program terminated... */
 }
