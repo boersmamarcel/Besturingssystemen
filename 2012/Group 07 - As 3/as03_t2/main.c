d661f956e6d1c9a1e7ed1d14df01f969734b08ad
Bram Leenders <aerkenbrand@gmail.com>
2013-01-17 11:43:24 +0100

    Created makefile & fixed layout

--- 2012/Group 07 - As 3/as03_t2/main.c
+++ 2012/Group 07 - As 3/as03_t2/main.c
@@ -36,29 +36,29 @@ int main(int argc, char **argv) {
 			
 			for (i = 0; node->val.d[i].val; ++i) {
 				
-				printf("%s\n", node->val.d[i].key);
 				if(strcmp(node->val.d[i].key, "title") == 0) {
 					feedTitle = node->val.d[i].val->val.s;
+					printf("Feed title: %s\n", feedTitle);
 				}else if(strncmp(node->val.d[i].key, "item", 4) == 0){
-                    
+					printf("New item:\n");
 					int j;
 					for(j = 0; node->val.d[i].val->val.d[j].val; ++j) {
 						//~ The dictionary we're currently exploring
 						be_dict currDict = node->val.d[i].val->val.d[j];					
 						 
 						if(strcmp(currDict.key, "title") == 0){
-							printf("title: %s\n", currDict.val->val.s);
+							printf("\ttitle: %s\n", currDict.val->val.s);
 						}else if(strcmp(currDict.key, "description") == 0){
-							printf("description: %s\n", currDict.val->val.s);
+							printf("\tdescription: %s\n", currDict.val->val.s);
 						}else if(strcmp(currDict.key, "link") == 0){
-							printf("link: %s\n", currDict.val->val.s);
+							printf("\tlink: %s\n", currDict.val->val.s);
 						}else if(strcmp(currDict.key, "guid") == 0){
-							printf("guid: %s\n", currDict.val->val.s);
-						}
-						else if(strcmp(currDict.key, "pubDate") == 0){
-							printf("pubDate: %s\n", currDict.val->val.s);
+							printf("\tguid: %s\n", currDict.val->val.s);
+						}else if(strcmp(currDict.key, "pubDate") == 0){
+							printf("\tpubDate: %s\n", currDict.val->val.s);
 						}
 					 }
+					 printf("\n");
 				}
 			}
 		}
