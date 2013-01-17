9ecc043ee100a3bde594513b9482a4264ba63d99
Bram Leenders <aerkenbrand@gmail.com>
2013-01-17 10:46:27 +0100

    feedTitle changed & shorter code
    replaced feedTitle & shortened code

--- 2012/Group 07 - As 3/as03_t2/main.c
+++ 2012/Group 07 - As 3/as03_t2/main.c
@@ -38,23 +38,24 @@ int main(int argc, char **argv) {
 				
 				printf("%s\n", node->val.d[i].key);
 				if(strcmp(node->val.d[i].key, "title") == 0) {
-					feedTitle = node->val.d[i].key;
+					feedTitle = node->val.d[i].val->val.s;
+					printf(">>>%s\n", feedTitle);
 				}else if(strncmp(node->val.d[i].key, "item", 4) == 0){
                     
 					int j;
-					 for(j = 0; node->val.d[i].val->val.d[j].val; ++j) {
-                         
-                         if(strcmp(node->val.d[i].val->val.d[j].key, "title") == 0){
-                             printf("title: %s\n", node->val.d[i].val->val.d[j].val->val.s);
-                         }else if(strcmp(node->val.d[i].val->val.d[j].key, "description") == 0){
-                             printf("description: %s\n", node->val.d[i].val->val.d[j].val->val.s);
-                         }else if(strcmp(node->val.d[i].val->val.d[j].key, "link") == 0){
-                             printf("link: %s\n", node->val.d[i].val->val.d[j].val->val.s);
-                         }else if(strcmp(node->val.d[i].val->val.d[j].key, "guid") == 0){
-                             printf("guid: %s\n", node->val.d[i].val->val.d[j].val->val.s);
-                         }
-                         
-						
+					for(j = 0; node->val.d[i].val->val.d[j].val; ++j) {
+						//~ The dictionary we're currently exploring
+						be_dict currDict = node->val.d[i].val->val.d[j];					
+						 
+						if(strcmp(currDict.key, "title") == 0){
+							printf("title: %s\n", currDict.val->val.s);
+						}else if(strcmp(currDict.key, "description") == 0){
+							printf("description: %s\n", currDict.val->val.s);
+						}else if(strcmp(currDict.key, "link") == 0){
+							printf("link: %s\n", currDict.val->val.s);
+						}else if(strcmp(currDict.key, "guid") == 0){
+							printf("guid: %s\n", currDict.val->val.s);
+						}
 					 }
 				}
 			}
